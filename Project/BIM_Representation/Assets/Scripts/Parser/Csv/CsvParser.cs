609432ef0598d62f0be78c17d026333431bf22ad
Kronoxis <dev.kronoxis@gmail.com>
2017-10-19 16:54:01 +0200

    Updated CsvParser to use Helper function

--- Project/BIM_Representation/Assets/Scripts/Parser/Csv/CsvParser.cs
+++ Project/BIM_Representation/Assets/Scripts/Parser/Csv/CsvParser.cs
@@ -15,7 +15,7 @@ public class CsvParser
         StreamReader sr = new StreamReader(file);
         while (!sr.EndOfStream)
         {
-            var values = SplitCsvLine(sr.ReadLine());
+            var values = Helpers.SplitCsvLine(sr.ReadLine());
             if (Keys.Count == 0)
             {
                 Keys = values.ToList();
@@ -23,25 +23,5 @@ public class CsvParser
             }
             Values.Add(values);
         }
-    }
-
-    private string[] SplitCsvLine(string line)
-    {
-        string pattern = @"
-     # Match one value in valid CSV string.
-     (?!\s*$)                                      # Don't match empty last value.
-     \s*                                           # Strip whitespace before value.
-     (?:                                           # Group for value alternatives.
-       '(?<val>[^'\\]*(?:\\[\S\s][^'\\]*)*)'       # Either $1: Single quoted string,
-     | ""(?<val>[^""\\]*(?:\\[\S\s][^""\\]*)*)""   # or $2: Double quoted string,
-     | (?<val>[^,'""\s\\]*(?:\s+[^,'""\s\\]+)*)    # or $3: Non-comma, non-quote stuff.
-     )                                             # End group of value alternatives.
-     \s*                                           # Strip whitespace after value.
-     (?:,|$)                                       # Field ends on comma or EOS.
-     ";
-        string[] values = (from Match m in Regex.Matches(line, pattern,
-                RegexOptions.ExplicitCapture | RegexOptions.IgnorePatternWhitespace | RegexOptions.Multiline)
-                           select m.Groups[1].Value).ToArray();
-        return values;
     }
 }
